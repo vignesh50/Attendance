a213f30bd4b695011b42d916d023605f35515185
vignesh50 <128451342+vignesh50@users.noreply.github.com>
2023-11-17 15:17:46 +0530

    Update Attendance.py
    File store Feature are added in Attendance Application

--- Attendance.py
+++ Attendance.py
@@ -7,15 +7,23 @@ studentList = ['Abinaya.N','Akash.S','M.Aswin']
 present = 0 
 absent = 0
 
+file_name = "DayOne.txt"
+file = open(file_name,'w')
+
 for x in studentList:
     PrtOrAbs = input(x + " is Present Today: ")
 
     if PrtOrAbs == "p":
         print(x + " is Present")
         present+=1
+        file.write(x + " is Present\n")
     else:
         print(x + " is Absent")
         absent+=1
+        file.write(x + " is Absent\n")
+
+file.close()
+print("\nFile saved Sucessfully\n")
 
 print("\nTotal Class Present\n")
 print("Today Present is: " + str(present) + " and Absent is: " + str(absent) + "\n")
